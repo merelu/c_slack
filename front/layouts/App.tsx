07a804d18f4809c20a37696221b1ac5578ad724c
GYUHA_HWANG <gyuha0310@gmail.com>
2021-02-21 20:13:23 +0900

    react router setting and test

--- front/layouts/App.tsx
+++ front/layouts/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Login from '@pages/Login';
+import SignUp from '@pages/SignUp';
 
-const App = () => {
-  return <div>핫 리로딩</div>;
-};
+function App() {
+  return (
+    <Switch>
+      <Redirect exact path="/" to="/login" />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={SignUp} />
+    </Switch>
+  );
+}
 
 export default App;
 
