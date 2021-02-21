8dd8122f3f56cf981cab2e2dde213cebcfbc5a7d
GYUHA_HWANG <gyuha0310@gmail.com>
2021-02-21 20:24:25 +0900

    code split page 기준으로

--- front/layouts/App.tsx
+++ front/layouts/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import loadable from '@loadable/component';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import Login from '@pages/Login';
-import SignUp from '@pages/SignUp';
+const Login = loadable(() => import('@pages/Login'));
+const SignUp = loadable(() => import('@pages/SignUp'));
 
 function App() {
   return (
