10ff96a41f38182de8078a808c4dc3d437e004ee
GYUHA_HWANG <gyuha0310@gmail.com>
2021-02-26 19:46:19 +0900

    folder struct change

--- front/client.tsx
+++ front/client.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from '@layouts/App';
 import { BrowserRouter } from 'react-router-dom';
+import App from '@layouts/App';
 
 render(
   <BrowserRouter>
