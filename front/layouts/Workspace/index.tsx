374df0cc2dd4a99d1e9b5ed63648ab27f301afd1
GYUHA_HWANG <gyuha0310@gmail.com>
2021-02-27 14:31:11 +0900

    add workspace layout

--- front/layouts/Workspace/index.tsx
+++ front/layouts/Workspace/index.tsx
@@ -1,8 +1,26 @@
 import fetcher from '@utils/fetcher';
 import axios from 'axios';
 import React, { FC, ReactNode, useCallback } from 'react';
-import { Redirect } from 'react-router';
+import { Redirect, Route, Switch } from 'react-router';
 import useSWR from 'swr';
+import {
+  Channels,
+  Chats,
+  Header,
+  LogOutButton,
+  MenuScroll,
+  ProfileImg,
+  RightMenu,
+  WorkspaceName,
+  Workspaces,
+  WorkspaceWrapper,
+} from './styles';
+import gravatar from 'gravatar';
+import loadable from '@loadable/component';
+
+const Channel = loadable(() => import('@pages/Channel'));
+const DirectMessage = loadable(() => import('@pages/DirectMessage'));
+
 type TWorkspaceProps = {
   children: ReactNode;
 };
@@ -19,8 +37,29 @@ function Workspace({ children }: TWorkspaceProps) {
   }
   return (
     <div>
+      <Header>
+        {userData && (
+          <RightMenu>
+            <span>
+              <ProfileImg src={gravatar.url(userData.nickname, { s: '28px', d: 'retro' })} alt={userData.nickname} />
+            </span>
+          </RightMenu>
+        )}
+      </Header>
+      <WorkspaceWrapper>
+        <Workspaces>test</Workspaces>
+        <Channels>
+          <WorkspaceName>C_slack</WorkspaceName>
+          <MenuScroll>MenuScroll</MenuScroll>
+        </Channels>
+        <Chats>
+          <Switch>
+            <Route path="/workspace/channel" component={Channel} />
+            <Route path="/workspace/dm" component={DirectMessage} />
+          </Switch>
+        </Chats>
+      </WorkspaceWrapper>
       <button onClick={onLogout}>로그아웃</button>
-      {children}
     </div>
   );
 }
