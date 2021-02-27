d94bcf079adc26058a926aa6e4211279d6894120
GYUHA_HWANG <gyuha0310@gmail.com>
2021-02-27 14:31:44 +0900

    add DM init

---
+++ front/pages/DirectMessage/index.tsx
@@ -0,0 +1,14 @@
+import Workspace from '@layouts/Workspace';
+import { Header } from '@layouts/Workspace/styles';
+import React from 'react';
+import { Container } from './styles';
+
+function DirectMessage() {
+  return (
+    <Container>
+      <Header>DM</Header>
+    </Container>
+  );
+}
+
+export default DirectMessage;
