7e0d8cb69e9be7a0093396b9d4ffad832cebdf42
GYUHA_HWANG <gyuha0310@gmail.com>
2021-02-28 21:04:42 +0900

    add Modal component

---
+++ front/components/Modal/index.tsx
@@ -0,0 +1,28 @@
+import React, { ReactNode, useCallback } from 'react';
+import { CreateModal, CloseModalButton } from './styles';
+
+interface IModalProps {
+  children: ReactNode;
+  show: boolean;
+  onCloseModal: () => void;
+}
+function Modal({ children, show, onCloseModal }: IModalProps) {
+  const stopPropagation = useCallback((e) => {
+    e.stopPropagation();
+  }, []);
+
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <CreateModal onClick={onCloseModal}>
+      <div onClick={stopPropagation}>
+        <CloseModalButton onClick={onCloseModal}>&times;</CloseModalButton>
+        {children}
+      </div>
+    </CreateModal>
+  );
+}
+
+export default Modal;
