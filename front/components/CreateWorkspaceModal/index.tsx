01e663bdd393d75e9c31244f11a0371c12ca7ba9
GYUHA_HWANG <gyuha0310@gmail.com>
2021-03-01 21:04:55 +0900

    add createWorkspaceModal component

---
+++ front/components/CreateWorkspaceModal/index.tsx
@@ -0,0 +1,66 @@
+import useInput from '@hooks/useInput';
+import { Button, Input, Label } from '@pages/SignUp/styles';
+import { IUser } from '@typings/db';
+import fetcher from '@utils/fetcher';
+import axios from 'axios';
+import React, { useCallback } from 'react';
+import { toast } from 'react-toastify';
+import useSWR from 'swr';
+import Modal from '../Modal';
+
+interface ICreateWorkspaceModalProps {
+  show: boolean;
+  onCloseModal: () => void;
+}
+
+function CreateWorkspaceModal({ show, onCloseModal }: ICreateWorkspaceModalProps) {
+  const { revalidate: revalidateUser } = useSWR<IUser | false>('http://localhost:3095/api/users', fetcher);
+  const [newWorkspace, onChangeNewWorkspace, setNewWorkspace] = useInput('');
+  const [newUrl, onChangeNewUrl, setNewUrl] = useInput('');
+
+  const onCreateWorkspace = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (!newWorkspace || !newWorkspace.trim()) return;
+      if (!newUrl || !newUrl.trim()) return;
+      axios
+        .post(
+          'http://localhost:3095/api/workspaces',
+          {
+            workspace: newWorkspace,
+            url: newUrl,
+          },
+          { withCredentials: true },
+        )
+        .then(() => {
+          revalidateUser();
+          onCloseModal();
+          setNewWorkspace('');
+          setNewUrl('');
+          toast.success('워크스페이스 생성 성공!');
+        })
+        .catch((error) => {
+          console.dir(error);
+          toast.error(error.response?.data, { position: 'bottom-center' });
+        });
+    },
+    [newWorkspace, newUrl],
+  );
+  return (
+    <Modal show={show} onCloseModal={onCloseModal}>
+      <form onSubmit={onCreateWorkspace}>
+        <Label id="workspace-label">
+          <span>워크스페이스 이름</span>
+          <Input id="workspace" value={newWorkspace} onChange={onChangeNewWorkspace} />
+        </Label>
+        <Label id="workspace-url-label">
+          <span>워크스페이스 url</span>
+          <Input id="workspace" value={newUrl} onChange={onChangeNewUrl} />
+        </Label>
+        <Button type="submit">생성하기</Button>
+      </form>
+    </Modal>
+  );
+}
+
+export default CreateWorkspaceModal;
