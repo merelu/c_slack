0bc77584ac850c0a10f61105c80ede17e6655328
GYUHA_HWANG <gyuha0310@gmail.com>
2021-03-03 23:59:09 +0900

    add Directmessage page init ver

--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -1,11 +1,23 @@
-import { Header } from '@layouts/Workspace/styles';
+import fetcher from '@utils/fetcher';
 import React from 'react';
-import { Container } from './styles';
+import useSWR from 'swr';
+import { Container, Header } from './styles';
+import gravatar from 'gravatar';
+import { useParams } from 'react-router';
 
 function DirectMessage() {
+  const { workspace, id } = useParams<{ workspace: string; id: string }>();
+  const { data: myData } = useSWR('/api/users', fetcher);
+  const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
+  console.log('userdata', userData);
+
+  if (!myData || !userData) return null;
   return (
     <Container>
-      <Header>DM</Header>
+      <Header>
+        <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
+        <span>{userData.nickname}</span>
+      </Header>
     </Container>
   );
 }
