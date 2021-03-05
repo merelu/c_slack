1c12609476a2fb7abae82ded8b63369de9517051
GYUHA_HWANG <gyuha0310@gmail.com>
2021-03-05 13:39:25 +0900

    add chatBox component

--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -1,16 +1,23 @@
 import fetcher from '@utils/fetcher';
-import React from 'react';
+import React, { useCallback } from 'react';
 import useSWR from 'swr';
 import { Container, Header } from './styles';
 import gravatar from 'gravatar';
 import { useParams } from 'react-router';
+import ChatBox from '@components/ChatBox';
+import ChatList from '@components/ChatList';
+import useInput from '@hooks/useInput';
 
 function DirectMessage() {
   const { workspace, id } = useParams<{ workspace: string; id: string }>();
   const { data: myData } = useSWR('/api/users', fetcher);
   const { data: userData } = useSWR(`/api/workspaces/${workspace}/members/${id}`, fetcher);
-  console.log('userdata', userData);
+  const [chat, onChangeChat, setChat] = useInput('');
 
+  const onSubmitForm = useCallback((e) => {
+    e.preventDefault();
+    console.log('submit');
+  }, []);
   if (!myData || !userData) return null;
   return (
     <Container>
@@ -18,8 +25,8 @@ function DirectMessage() {
         <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
         <span>{userData.nickname}</span>
       </Header>
-      {/* <ChatList />
-      <ChatBox /> */}
+      <ChatList />
+      <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
     </Container>
   );
 }
