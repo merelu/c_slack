ebba479db4f3c84dfd4474ab84d154effcea89ef
GYUHA_HWANG <gyuha0310@gmail.com>
2021-03-05 13:40:16 +0900

    add ChatList component and init

---
+++ front/components/ChatList/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ChatZone, Section, StickyHeader } from './styles';
+
+function ChatList() {
+  return (
+    <ChatZone>
+      <Section>
+        <StickyHeader>stickyheader</StickyHeader>
+        section
+      </Section>
+    </ChatZone>
+  );
+}
+export default ChatList;
