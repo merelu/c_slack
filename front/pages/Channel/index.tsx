d36b37b9c7595036dc96fc47176463a04ef006bf
GYUHA_HWANG <gyuha0310@gmail.com>
2021-03-05 13:39:52 +0900

    add channel pages and init

--- front/pages/Channel/index.tsx
+++ front/pages/Channel/index.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import ChatBox from '@components/ChatBox';
+import ChatList from '@components/ChatList';
+import useInput from '@hooks/useInput';
+import React, { useCallback } from 'react';
 import { Container, Header } from './styles';
 
 function Channel() {
+  const [chat, onChangeChat, setChat] = useInput('');
+
+  const onSubmitForm = useCallback((e) => {
+    e.preventDefault();
+    console.log(chat);
+  }, []);
   return (
     <Container>
       <Header>Channels</Header>
+      <ChatList></ChatList>
+      <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
     </Container>
   );
 }
