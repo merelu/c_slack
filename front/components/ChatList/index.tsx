3f3344d9ab1e0419d557d3585206c44b6aed526d
GYUHA_HWANG <gyuha0310@gmail.com>
2021-03-07 21:39:22 +0900

    apply scrollbar and dayjs

--- front/components/ChatList/index.tsx
+++ front/components/ChatList/index.tsx
@@ -1,20 +1,21 @@
 import Chat from '@components/Chat';
 import { IChat, IDM } from '@typings/db';
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { ChatZone, Section, StickyHeader } from './styles';
 import { Scrollbars } from 'react-custom-scrollbars';
 interface IChatList {
   chatData?: IDM[];
 }
 function ChatList({ chatData }: IChatList) {
-  if (chatData) {
-    console.log(chatData);
-  }
+  const scrollbarRef = useRef(null);
+  const onScroll = useCallback(() => {}, []);
   return (
     <ChatZone>
-      {chatData?.map((chat) => (
-        <Chat key={chat.id} data={chat} />
-      ))}
+      <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
+        {chatData?.map((chat) => (
+          <Chat key={chat.id} data={chat} />
+        ))}
+      </Scrollbars>
     </ChatZone>
   );
 }
