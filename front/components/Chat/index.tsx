7d5c6ed6a24e7f45883ae1e9fad5997a84448090
GYUHA_HWANG <gyuha0310@gmail.com>
2021-03-08 19:20:27 +0900

    modified Chat component use regexifyString

--- front/components/Chat/index.tsx
+++ front/components/Chat/index.tsx
@@ -1,15 +1,34 @@
 import React from 'react';
 import { ChatWrapper } from './styles';
 import gravatar from 'gravatar';
-import { IDM, IChat, IUser } from '@typings/db';
+import { IDM } from '@typings/db';
 import dayjs from 'dayjs';
-
+import regexifyString from 'regexify-string';
+import { Link, useParams } from 'react-router-dom';
 interface IChatProps {
   data: IDM;
 }
 function Chat({ data }: IChatProps) {
+  const { workspace } = useParams<{ workspace: string }>();
   const user = data.Sender;
-  console.log('chat');
+
+  // @[gyuha](2)
+  const result = regexifyString({
+    input: data.content,
+    pattern: /@\[(.+?)]\((\d+?)\)|\n/g,
+    decorator(match, index) {
+      const arr: string[] | null = match.match(/@\[(.+?)]\((\d+?)\)/)!;
+      if (arr) {
+        return (
+          <Link key={match + index} to={`/workspace/${workspace}/dm/${arr[2]}`}>
+            @{arr[1]}
+          </Link>
+        );
+      }
+      return <br key={index} />;
+    },
+  });
+
   return (
     <ChatWrapper>
       <div className="chat-img">
@@ -20,7 +39,7 @@ function Chat({ data }: IChatProps) {
           <b>{user.nickname}</b>
           <span>{dayjs(data.createdAt).format('h:mm A')}</span>
         </div>
-        <p>{data.content}</p>
+        <p>{result}</p>
       </div>
     </ChatWrapper>
   );
