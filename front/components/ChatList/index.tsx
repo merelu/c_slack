7e2b91e0d5e1ebc53db77a3243c60365dc6cc56f
GYUHA_HWANG <gyuha0310@gmail.com>
2021-03-09 21:09:30 +0900

    apply Chat scrollbar section and scrollbar pos

--- front/components/ChatList/index.tsx
+++ front/components/ChatList/index.tsx
@@ -1,20 +1,40 @@
 import Chat from '@components/Chat';
-import { IChat, IDM } from '@typings/db';
-import React, { useCallback, useRef } from 'react';
+import { IDM } from '@typings/db';
+import React, { useCallback, useRef, forwardRef, RefObject } from 'react';
 import { ChatZone, Section, StickyHeader } from './styles';
 import { Scrollbars } from 'react-custom-scrollbars';
 interface IChatList {
-  chatData?: IDM[];
+  chatSections: { [key: string]: IDM[] };
+  setSize: (size: number | ((size: number) => number)) => Promise<IDM[][] | undefined>;
+  scrollbarRef: RefObject<Scrollbars>;
+  isEmpty: boolean;
+  isReachingEnd: boolean;
 }
-function ChatList({ chatData }: IChatList) {
-  const scrollbarRef = useRef(null);
-  const onScroll = useCallback(() => {}, []);
+function ChatList({ chatSections, setSize, scrollbarRef, isEmpty, isReachingEnd }: IChatList) {
+  const onScroll = useCallback((values) => {
+    if (values.scrollTop === 0 && !isReachingEnd) {
+      console.log('가장 위');
+      setSize((prevSize) => prevSize + 1).then(() => {
+        //스크롤 위치 유지
+        scrollbarRef.current?.scrollTop(scrollbarRef.current?.getScrollHeight() - values.scrollHeight);
+      });
+    }
+  }, []);
   return (
     <ChatZone>
       <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
-        {chatData?.map((chat) => (
-          <Chat key={chat.id} data={chat} />
-        ))}
+        {Object.entries(chatSections).map(([date, chats]) => {
+          return (
+            <Section className={`section-${date}`} key={date}>
+              <StickyHeader>
+                <button>{date}</button>
+              </StickyHeader>
+              {chats.map((chat) => (
+                <Chat key={chat.id} data={chat} />
+              ))}
+            </Section>
+          );
+        })}
       </Scrollbars>
     </ChatZone>
   );
